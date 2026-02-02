f916fdf11f3ec9b1d6b045b72e1da9600befda14
Arunjot-ATR592 <arunjot@antiersolutions.com>
2026-02-02 10:51:22 +0530

    chore: Added code for linear search in array

---
+++ src/array/linear_search.rs
@@ -0,0 +1,31 @@
+fn linear() {
+    let arr = [1,2,44,53];
+    let target = 98;
+    let position = linear_search(&arr,target);
+    println!("Postion is {:?}",position)
+}
+
+fn linear_search(arr: &[i32], target: i32) -> i32{
+    let mut index = -1;
+    
+    let mut check_index_aval = 0;
+    
+    for i in arr{
+        if *i == target{
+            check_index_aval +=1;
+        }
+    }
+    
+    if check_index_aval <= 0{
+       return -1
+    }
+            
+    for i in arr {
+        index = index+1;
+        if *i == target {
+           break;
+        }
+    }
+    index
+    
+}
